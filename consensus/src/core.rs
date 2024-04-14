6ef95ee3cb861471af47b2e630716b1c305779a7
neilgiri <neilgiri88@gmail.com>
2024-04-14 10:32:19 -0700

    revert propose delay queue

--- consensus/src/core.rs
+++ consensus/src/core.rs
@@ -102,7 +102,6 @@ pub struct Core {
     //egress_timer_futures: FuturesUnordered<Pin<Box<dyn Future<Output = ()> + Send>>>,
     egress_delay_queue: DelayQueue<(ConsensusMessage, Option<PublicKey>)>,
     current_egress_end: Instant,
-    propose_delay_queue: DelayQueue<Option<TC>>,
     // Timeouts
     use_exponential_timeouts: bool,
     original_timeout: u64,
@@ -172,7 +171,6 @@ impl Core {
             //egress_delayed_msgs: VecDeque::new(),
             egress_delay_queue: DelayQueue::new(),
             current_egress_end: Instant::now(),
-            propose_delay_queue: DelayQueue::new(),
             //egress_timer_futures: FuturesUnordered::new(),
             use_exponential_timeouts,
             original_timeout: timeout_value,
@@ -399,8 +397,7 @@ impl Core {
 
             // Make a new block if we are the next leader.
             if self.name == self.leader_elector.get_leader(self.round) {
-                let egress_path = self.during_simulated_asynchrony && self.current_effect_type == AsyncEffectType::Egress;
-                self.generate_proposal(None, egress_path).await?;
+                self.generate_proposal(None).await?;
             }
         }
         Ok(())
@@ -439,8 +436,7 @@ impl Core {
 
             // Make a new block if we are the next leader.
             if self.name == self.leader_elector.get_leader(self.round) {
-                let egress_path = self.during_simulated_asynchrony && self.current_effect_type == AsyncEffectType::Egress;
-                self.generate_proposal(Some(tc), egress_path).await?;
+                self.generate_proposal(Some(tc)).await?;
             }
         }
         Ok(())
@@ -462,17 +458,7 @@ impl Core {
     // -- End Pacemaker --
 
     #[async_recursion]
-    async fn generate_proposal(&mut self, tc: Option<TC>, egress_path: bool) -> ConsensusResult<()> {
-        if egress_path {
-            let egress_end_time = Instant::now().checked_add(Duration::from_millis(self.egress_penalty)).unwrap();
-            debug!("egress penalty is {:?}", self.egress_penalty);
-            debug!("propose msg egress end time is {:?}", egress_end_time);
-            let actual_send_time = egress_end_time.min(self.current_egress_end);
-            debug!("msg actual send time is {:?}", actual_send_time);
-
-            self.propose_delay_queue.insert_at(tc.clone(), Instant::now().checked_add(Duration::from_millis(self.egress_penalty)).unwrap());
-            return Ok(())
-        }
+    async fn generate_proposal(&mut self, tc: Option<TC>) -> ConsensusResult<()> {
         /*if self.during_simulated_asynchrony {
             debug!("Simulating asynchrony skipping sending a block in round {:?}, will trigger view change", self.round);
             self.async_last_tc = tc;
@@ -650,8 +636,7 @@ impl Core {
     async fn handle_tc(&mut self, tc: TC) -> ConsensusResult<()> {
         self.advance_round(tc.round).await;
         if self.name == self.leader_elector.get_leader(self.round) {
-            let egress_path = self.during_simulated_asynchrony && self.current_effect_type == AsyncEffectType::Egress;
-            self.generate_proposal(Some(tc), egress_path).await?;
+            self.generate_proposal(Some(tc)).await?;
         }
         Ok(())
     }
@@ -775,7 +760,7 @@ impl Core {
         // Also, schedule a timer in case we don't hear from the leader.
         self.timer.reset();
         if self.name == self.leader_elector.get_leader(self.round) {
-            self.generate_proposal(None, false)
+            self.generate_proposal(None)
                 .await
                 .expect("Failed to send the first block");
         }
@@ -868,12 +853,6 @@ impl Core {
                     debug!("egress msg expired, sending normally");
                     let (message, author) = item.into_inner();
                     self.send_msg_normal(message, author).await;
-                    Ok(())
-                },
-
-                Some(item) = self.propose_delay_queue.next() => {
-                    let tc = item.into_inner();
-                    let _ = self.generate_proposal(tc, false).await;
                     Ok(())
                 },
 
