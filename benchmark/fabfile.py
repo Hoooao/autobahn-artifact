73cb7812ac98e6c26131e7bc3867b6c5f9c66bf7
neilgiri <neilgiri88@gmail.com>
2024-08-21 16:49:15 -0700

    update localbench params

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -13,34 +13,41 @@ from benchmark.remote import Bench, BenchError
 def local(ctx, debug=True):
     ''' Run benchmarks on localhost '''
     bench_params = {
-        'faults': 0, 
-        'nodes': 4,
+        'faults': 0,
+        'nodes': [4],
         'workers': 1,
-        'rate': 50_000,
+        'co-locate': True,
+        'rate': [240_000],
         'tx_size': 512,
-        'duration': 10,
+        'duration': 60,
+        'runs': 1,
+
+        # Unused
+        'simulate_partition': True,
+        'partition_start': 5,
+        'partition_duration': 5,
+        'partition_nodes': 1,
     }
     node_params = {
-        'timeout_delay': 500,  # ms
+        'timeout_delay': 5_000,  # ms
         'header_size': 32,  # bytes
-        'max_header_delay': 200,  # ms
+        'max_header_delay': 5_000,  # ms
         'gc_depth': 50,  # rounds
         'sync_retry_delay': 5_000,  # ms
         'sync_retry_nodes': 3,  # number of nodes
         'batch_size': 500_000,  # bytes
-        'max_batch_delay': 10,  # ms
-
+        'max_batch_delay': 20,  # ms
         'use_optimistic_tips': True,
         'use_parallel_proposals': True,
         'k': 4,
-        'use_fast_path': False,
-        'fast_path_timeout': 500,
+        'use_fast_path': True,
+        'fast_path_timeout': 5_000,
         'use_ride_share': False,
-        'car_timeout': 2000,
+        'car_timeout': 5_000,
 
-        'simulate_asynchrony': False, 
-        'asynchrony_start': 2_000, #ms
-        'asynchrony_duration': 1_000 #ms
+        'simulate_asynchrony': False,
+        'asynchrony_start': 15_000, #ms
+        'asynchrony_duration': 3_000, #ms
     }
     try:
         ret = LocalBench(bench_params, node_params).run(debug)
