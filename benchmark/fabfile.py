90044c306cd015c1f8c6f1714016a8929725bbd5
neilgiri <neilgiri88@gmail.com>
2024-08-21 16:56:37 -0700

    update fabfile

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -108,22 +108,33 @@ def remote(ctx, debug=False):
     ''' Run benchmarks on AWS '''
     bench_params = {
         'faults': 0,
-        'nodes': [10, 20],
+        'nodes': [4],
         'workers': 1,
-        'collocate': True,
-        'rate': [10_000, 50_000],
+        'co-locate': True,
+        'rate': [240_000],
         'tx_size': 512,
-        'duration': 300,
-        'runs': 2,
+        'duration': 60,
+        'runs': 1,
+
+        # Unused
+        'simulate_partition': True,
+        'partition_start': 5,
+        'partition_duration': 5,
+        'partition_nodes': 1,
     }
     node_params = {
-        'header_size': 50,  # bytes
+        'timeout_delay': 5_000,  # ms
+        'header_size': 32,  # bytes
         'max_header_delay': 5_000,  # ms
         'gc_depth': 50,  # rounds
-        'sync_retry_delay': 10_000,  # ms
+        'sync_retry_delay': 5_000,  # ms
         'sync_retry_nodes': 3,  # number of nodes
         'batch_size': 500_000,  # bytes
-        'max_batch_delay': 200  # ms
+        'max_batch_delay': 20,  # ms
+
+        'simulate_asynchrony': False,
+        'asynchrony_start': 15_000, #ms
+        'asynchrony_duration': 3_000, #ms
     }
     try:
         Bench(ctx).run(bench_params, node_params, debug)
