8ea47903004a9262e71593a8236fb1041a56fd78
neilgiri <neilgiri88@gmail.com>
2024-08-21 16:53:51 -0700

    update fabfile defaults

--- benchmark/fabfile.py
+++ benchmark/fabfile.py
@@ -11,25 +11,32 @@ from aws.remote import Bench, BenchError
 @task
 def local(ctx):
     ''' Run benchmarks on localhost '''
-
     bench_params = {
-        'nodes': 4,
-        'rate': 50_000,
+        'nodes': [4],
+        'rate': [15_000],
         'tx_size': 512,
         'faults': 0,
-        'duration': 20,
+        'duration': 60,
+        'runs': 1,
     }
     node_params = {
         'consensus': {
             'timeout_delay': 1_000,
-            'sync_retry_delay': 10_000,
-            'max_payload_size': 5000,
-            'min_block_delay': 0
+            'sync_retry_delay': 1_000,
+            'max_payload_size': 7_812_500,
+            'min_block_delay': 0,
+            'simulate_asynchrony': False,
+            'async_type': [3],
+            'asynchrony_start': [10_000],
+            'asynchrony_duration': [20_000],
+            'affected_nodes': [2],
+            'egress_penalty': 50,
+            'use_exponential_timeouts': False,
         },
         'mempool': {
-            'queue_capacity': 10_000,
-            'sync_retry_delay': 100_000,
-            'max_payload_size': 15_000,
+            'queue_capacity': 10_000_000,
+            'sync_retry_delay': 1_000,
+            'max_payload_size': 500_000,
             'min_block_delay': 0
         }
     }
@@ -99,7 +106,7 @@ def remote(ctx):
     ''' Run benchmarks on AWS '''
     bench_params = {
         'nodes': [4],
-        'rate': [10_000],
+        'rate': [15_000],
         'tx_size': 512,
         'faults': 0,
         'duration': 60,
@@ -107,17 +114,21 @@ def remote(ctx):
     }
     node_params = {
         'consensus': {
-            'timeout_delay': 5_000,
-            'sync_retry_delay': 5_000,
+            'timeout_delay': 1_000,
+            'sync_retry_delay': 1_000,
             'max_payload_size': 7_812_500,
             'min_block_delay': 0,
             'simulate_asynchrony': True,
-            'asynchrony_start': 15_000,
-            'asynchrony_duration': 30_000
+            'async_type': [3],
+            'asynchrony_start': [10_000],
+            'asynchrony_duration': [20_000],
+            'affected_nodes': [2],
+            'egress_penalty': 50,
+            'use_exponential_timeouts': False,
         },
         'mempool': {
             'queue_capacity': 10_000_000,
-            'sync_retry_delay': 5_000,
+            'sync_retry_delay': 1_000,
             'max_payload_size': 500_000,
             'min_block_delay': 0
         }
